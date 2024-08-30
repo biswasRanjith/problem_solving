13ea6ffe42350da617e66228a33e9a8ea87f736d
Ranjith <ranjith@Ranjiths-MacBook-Pro.local>
2024-08-30 19:50:18 +0100

    practice

--- sum.js
+++ sum.js
@@ -41,14 +41,19 @@ function charCount(str){
   
   var count = 1;
   let result = '';
-  for(var i =1;i<=str.length;i++){
-    if(str[i] == str[i-1]){
-      count++
-    }else{
-        console.log(str[i-1]+count)
-        count = 1;
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i - 1]) {
+        count++;
+    } else {
+        result += str[i - 1] + count + " ";
+        count = 1; // Reset count for the new character
     }
-  }
+}
+
+// Append the last character and its count
+result += str[str.length - 1] + count;
+
+return result;
 }
 
 var str = 'sssmmmnnnAAAmmmmmaaaaAAAAA';
